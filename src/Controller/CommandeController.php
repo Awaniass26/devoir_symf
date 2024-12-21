cf5d05c288523e2e857e41e9398e5fd4280255db
Awaniass@26 <hawamaama002@gmail.com>
2024-12-21 16:48:11 +0000

    exam

---
+++ src/Controller/CommandeController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Commande;
+use App\Entity\Client;
+use App\Entity\Article;
+use App\Repository\ClientRepository;
+use App\Repository\ArticleRepository;
+use App\Repository\CommandeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class CommandeController extends AbstractController
+{
+    
+    #[Route('/commande', name: 'commande_create', methods: ["GET", "POST"])]
+    public function createCommande(
+        Request $request,
+        ClientRepository $clientRepository,
+        ArticleRepository $articleRepository,
+        EntityManagerInterface $em
+    ): Response {
+        $client = null;
+        $commande = new Commande();
+        $total = 0;
+    
+        $articlesDisponibles = $articleRepository->findAll();
+    
+        if ($request->isMethod('POST')) {
+            $clientPhone = $request->get('phone');
+            $client = $clientRepository->findOneBy(['telephone' => $clientPhone]);
+    
+            if ($client) {
+                $commande->setClient($client);
+                $commande->setDateAt(new \DateTimeImmutable());
+    
+                $articleIds = $request->get('articles');  
+                $quantities = $request->get('quantity');  
+                $prices = $request->get('price');  
+    
+                if (is_array($articleIds) && count($articleIds) > 0) {
+                    foreach ($articleIds as $index => $id) {
+                        $article = $articleRepository->find($id);
+                        $quantity = $quantities[$index];
+                        $price = $prices[$index];
+    
+                        if ($article && $article->getQuantite() >= $quantity) {
+                            $commandeArticle = new CommandeArticle();
+                            $commandeArticle->setArticle($article);
+                            $commandeArticle->setQuantite($quantity);
+                            $commandeArticle->setPrix($price);  
+    
+                            $commande->addCommandeArticle($commandeArticle);
+    
+                            $article->setQuantite($article->getQuantite() - $quantity);
+    
+                            $total += $quantity * $price;  
+                        }
+                    }
+    
+                    $commande->setMontantTotal($total);
+    
+                    $em->persist($commande);
+                    $em->flush();
+    
+                    return $this->render('commande/new.html.twig', [
+                        'commande' => $commande,
+                        'client' => $client,
+                        'articles' => $articlesDisponibles,
+                        'total' => $total
+                    ]);
+                }
+            } else {
+                return $this->render('commande/new.html.twig', [
+                    'client_not_found' => true,
+                    'commande' => $commande,
+                    'client' => $client,
+                    'articles' => $articlesDisponibles,
+                    'total' => $total
+                ]);
+            }
+        }
+    
+        return $this->render('commande/new.html.twig', [
+            'commande' => $commande,
+            'client' => $client,
+            'articles' => $articlesDisponibles,
+            'total' => $total
+        ]);
+    }
+    
+    
+    
+     #[Route('/commande/delete_article/{id}', name:'commande_delete_article', methods:'POST')]
+    
+    public function deleteArticle(int $id, CommandeRepository $commandeRepository): Response
+    {
+        $commandeArticle = $commandeRepository->find($id);
+        if ($commandeArticle) {
+            $commande = $commandeArticle->getCommande();
+            $commande->removeArticle($commandeArticle);
+
+            $article = $commandeArticle->getArticle();
+            $article->setQuantite($article->getQuantite() + $commandeArticle->getQuantite());
+
+            $commandeRepository->save($commande);
+            return $this->redirectToRoute('commande_create');
+        }
+
+        return $this->redirectToRoute('commande_create');
+    }
+
+
+    
+     #[Route('/commande/update_article/{id}', name:'commande_update_article', methods:"POST")]
+    
+    public function updateArticle(int $id, Request $request, CommandeRepository $commandeRepository): Response
+    {
+        $commandeArticle = $commandeRepository->find($id);
+        if ($commandeArticle) {
+            $newPrice = $request->get('price');
+            $newQuantity = $request->get('quantity');
+
+            if ($newQuantity > 0 && $commandeArticle->getArticle()->getQuantite() >= $newQuantity) {
+                $commandeArticle->setPrix($newPrice);
+                $commandeArticle->setQuantite($newQuantity);
+
+                $article = $commandeArticle->getArticle();
+                $article->setQuantite($article->getQuantite() - $newQuantity);
+
+                $commandeRepository->save($commandeArticle->getCommande());
+            }
+
+            return $this->redirectToRoute('commande_create');
+        }
+
+        return $this->redirectToRoute('commande_create');
+    }
+}
+
---
+++ src/Entity/Commande.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commandes')]
+    private ?Client $client = null;
+
+    #[ORM\Column]
+    private ?float $montantTotal = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $dateAt = null;
+
+    /**
+     * @var Collection<int, CommandeArticle>
+     */
+    #[ORM\OneToMany(targetEntity: CommandeArticle::class, mappedBy: 'commande')]
+    private Collection $commandeArticles;
+
+    public function __construct()
+    {
+        $this->commandeArticles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getClient(): ?Client
+    {
+        return $this->client;
+    }
+
+    public function setClient(?Client $client): static
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    public function getMontantTotal(): ?float
+    {
+        return $this->montantTotal;
+    }
+
+    public function setMontantTotal(float $montantTotal): static
+    {
+        $this->montantTotal = $montantTotal;
+
+        return $this;
+    }
+
+    public function getDateAt(): ?\DateTimeImmutable
+    {
+        return $this->dateAt;
+    }
+
+    public function setDateAt(\DateTimeImmutable $dateAt): static
+    {
+        $this->dateAt = $dateAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CommandeArticle>
+     */
+    public function getCommandeArticles(): Collection
+    {
+        return $this->commandeArticles;
+    }
+
+    public function addCommandeArticle(CommandeArticle $commandeArticle): static
+    {
+        if (!$this->commandeArticles->contains($commandeArticle)) {
+            $this->commandeArticles->add($commandeArticle);
+            $commandeArticle->setCommande($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommandeArticle(CommandeArticle $commandeArticle): static
+    {
+        if ($this->commandeArticles->removeElement($commandeArticle)) {
+            // set the owning side to null (unless already changed)
+            if ($commandeArticle->getCommande() === $this) {
+                $commandeArticle->setCommande(null);
+            }
+        }
+
+        return $this;
+    }
+}
